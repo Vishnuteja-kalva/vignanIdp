0443157333cee72d96d53bbf69efb6cfdc26b959
Vishnuteja-kalva <vishnutejakalva@gmail.com>
2025-02-02 14:40:30 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState(localStorage.getItem("token") || "");
-    const url = "http://localhost:4000";
+    const url = "https://vignan-mhp.onrender.com";
 
     useEffect(() => {
         if (localStorage.getItem("token")) {
